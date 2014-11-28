b2fe2db997f7d63ddcd8f233e8f530e43da40e6b
c4osamam <c4osamam@23deeec9-4df3-419a-b266-fca847589da1>
2014-11-28 01:57:34 +0000

    Mapped image.

--- a3/ext2_helper.c
+++ a3/ext2_helper.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-#include <assert.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ext2.h>
+
+char *ext2_image;
+int fd;
+
+int read_image(char **filename){
+	struct stat image;
+
+	if ((fd = open(filename, O_RDWR)) == -1) {
+        perror("Opening image");
+        return 1;
+    }
+
+    fstat(fd, &image);
+
+    ext2_image = mmap(NULL, image.st_size, PROT_WRITE, MAP_SHARED, fd, 0)
+
+    if (ext_image = MAP_FAILED){
+    	perror("Mapping image");
+    	return 1;
+    }
+
+    return 0;
+}
+
